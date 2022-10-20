7bbcc3515b8d928d1f504bb866c208655fd6ea2b
Kristof <kristof@aut.bme.hu>
2022-10-20 13:58:42 +0200

    Hash codes test app

---
+++ hashcodes/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+string code = "ABC124";
+using(var sha = SHA1.Create())
+{
+    var hash = sha.ComputeHash(Encoding.UTF8.GetBytes(code));
+    var hashHex = Convert.ToHexString(hash);
+    var hashBase64 = Convert.ToBase64String(hash);
+    Console.WriteLine($"Hex: {hashHex}");
+    Console.WriteLine($"Base64: {hashBase64}");
+
+    var userID = hashBase64.Substring(0, 5).ToUpper();
+    Console.WriteLine($"UserID: {userID}");
+
+    const string salt = "X";
+
+    var password = Convert.ToBase64String(
+        sha.ComputeHash(Encoding.UTF8.GetBytes(userID + salt))).Substring(0,5).ToUpper();
+
+    Console.WriteLine($"Password: {password}");
+
+}
