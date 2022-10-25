280baee761aab5ac62dbaa85e97c3eeac78e700f
kristof <csorba.kristof@vik.bme.hu>
2022-10-25 09:47:51 +0200

    GenerateUserIdController works

---
+++ ScoreCaster/WebApp/Server/Controllers/GenerateUserIdController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Shared;
+
+namespace WebApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GenerateUserIdController : Controller
+    {
+        [HttpGet]
+        public UserIdAndPassword Get()
+        {
+            return new UserIdAndPassword() { UserID = "USER", Password = "PWD" };
+        }
+    }
+}
