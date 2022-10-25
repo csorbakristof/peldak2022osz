5222a1ca740e17188f7403c3040db547186785cc
kristof <csorba.kristof@vik.bme.hu>
2022-10-25 16:21:15 +0200

    ListReviews works without userID validation and filtering

---
+++ ScoreCaster/WebApp/Server/Controllers/ListReviewsController.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListReviewsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Response> Get(string userID, string password)
+        {
+            // TODO: validate username and password
+            // Filter on targetUserID
+            return Program.ServerSideDataAndServices.Questions.SelectMany(q => q.GetResponses());
+        }
+    }
+}
