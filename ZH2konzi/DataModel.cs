f3c6b5b67ad48ad4ec676e96ad3c6e64ab794c09
kristof <csorba.kristof@vik.bme.hu>
2022-12-01 15:05:13 +0100

    ZH2 konzi first version runs

---
+++ ZH2konzi/DataModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZH2konzi
+{
+    public class DataModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<NumberAndColor> List;
+
+        private NumberAndColor selectedItem;
+        public NumberAndColor SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedItem)));
+                }
+            }
+        }
+
+        public DataModel()
+        {
+            List = new ObservableCollection<NumberAndColor>();
+            List.Add(new NumberAndColor() { Number = 20 });
+            List.Add(new NumberAndColor() { Number = 40 });
+            List.Add(new NumberAndColor() { Number = 60 });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ZH2konzi/NumberAndColor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace ZH2konzi
+{
+    public class NumberAndColor : INotifyPropertyChanged
+    {
+        private readonly SolidColorBrush redBrush = new SolidColorBrush(Colors.Red);
+        private readonly SolidColorBrush greenBrush = new SolidColorBrush(Colors.Green);
+
+        private int number;
+        private SolidColorBrush brush;
+        public int Number
+        { 
+            get
+            {
+                return number;
+            }
+            set
+            {
+                if (number != value)
+                {
+                    number = value;
+                    Brush = number < 50 ? greenBrush : redBrush;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Number)));
+                }
+            }
+        }
+        public SolidColorBrush Brush
+        {
+            get
+            {
+                return brush;
+            }
+            set
+            {
+                if (brush != value)
+                {
+                    brush = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Brush)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
